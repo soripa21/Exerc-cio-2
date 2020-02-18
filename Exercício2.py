54eef3518c2d618bf43e0b3ce2389d40084ee575
soripa21 <61196649+soripa21@users.noreply.github.com>
2020-02-18 17:05:08 -0300

    Add files via upload

---
+++ Exercício2.py
@@ -0,0 +1,3 @@
+dias_semana = {'1': 'domingo', '2': 'segunda', '3': 'terça', '4': 'quarta', '5': 'quinta', '6': 'sexta', '7': 'sábado'}
+número = input('Digite o número: ')
+print(dias_semana[número])
